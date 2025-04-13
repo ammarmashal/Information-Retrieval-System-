7f9fd3053714afc4107db2694b8ef7b16cbe9ad8
ammarmashal <63562271+MAHMOUD-GAMAL-41@users.noreply.github.com>
2025-04-13 07:49:02 +0200

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Information Retrieval System', 
+    version= '0.0.0',
+    author= 'Ammar Yasser',
+    author_email= 'ammarmasha0155@gmailcom',
+    packages= find_packages(),
+    install_requires = []
+
+)
